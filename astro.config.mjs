7f05f08371931251a30805fe93b28d715bc41af7
Rsda23 <rsda23.0@gmail.com>
2026-01-08 09:34:05 +0100

    feat: add new sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  server: {
+    port: 4321,
+    host: true,
+  },
   vite: {
     plugins: [tailwindcss()]
   }
